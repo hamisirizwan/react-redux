8336fe602ef8b12a23a11c627921aa52613630dd
me <me@example.com>
2021-12-12 21:58:07 +0300

    some changes

--- src/components/Description.js
+++ src/components/Description.js
@@ -12,13 +12,18 @@ function Description() {
     phone: "0720532853",
   };
   const addNewUser = () => {
-    dispatch(setUser(userToAdd));
+    localStorage.setItem("user", JSON.stringify(userToAdd));
+    const currentUser = localStorage.getItem("user");
+    console.log(currentUser);
+    dispatch(setUser(JSON.parse(currentUser)));
   };
+
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <button onClick={addNewUser}>ADD USER</button>
+
       <h1>{user.name}</h1>
       <h1>{user.email}</h1>
       <h1>{user.phone}</h1>
